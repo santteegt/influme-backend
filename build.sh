6bbaa2aa78e8beb3e6fae0117a60207d741099f0
Santiago Gonzalez Toral <hernangt12re3@gmail.com>
2019-04-02 11:44:36 -0500

    Update build script for production

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ cd src && npm install && cd ..
 rm -rf dist && mkdir dist
 npx babel src --out-dir dist --ignore node_modules
 cp src/package.json dist
-cd dist && yarn install --production --modules-folder node_modules
+cd dist && npx yarn install --production --modules-folder node_modules
