b2ae63b35a44d93820025863f6fff1376c051231
Renan <renangfz@hotmail.com>
2019-06-02 22:11:58 -0500

    Minor Changes

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd src && npm install
-# cd client && npm install && npm run build
+cd client && npm install && npm run build
 # npx babel src --out-dir dist --ignore node_modules
 # cp src/package.json dist
 # cd dist && npx yarn install --production --modules-folder node_modules
